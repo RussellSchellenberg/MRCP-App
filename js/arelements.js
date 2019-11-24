f8f5275b87c304243a3beba056d0b5e8dad4fc76
Wright-Dj <wrightdj2000@gmail.com>
2019-11-23 20:05:39 -0800

    Add style and fix unreliable button presses

--- js/arelements.js
+++ js/arelements.js
@@ -1,32 +1,53 @@
-AFRAME.registerComponent('box', {
-    schema: {
-      width: {type: 'number', default: 1},
-      height: {type: 'number', default: 1},
-      depth: {type: 'number', default: 1},
-      color: {type: 'color', default: '#AAA'}
-    },
-  
-    /**
-     * Initial creation and setting of the mesh.
-     */
+AFRAME.registerComponent('panel', {
     init: function () {
-      var data = this.data;
-      var el = this.el;
-      console.log("done");
-  
-      // Create geometry.
-      this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
-  
-      // Create material.
-      this.material = new THREE.MeshStandardMaterial({color: data.color});
-  
-      // Create mesh.
-      this.mesh = new THREE.Mesh(this.geometry, this.material);
-  
-      // Set mesh on entity.
-      el.setObject3D('mesh', this.mesh);
+      this.parent = document.querySelector('a-marker').object3D;
+      this.shadow = document.querySelector('#test-panel');
+      this.canvas = document.querySelector('canvas');
     },
-    tick: function(){
-
-    },
+    tick: function(time, timeDelta){
+    this.el.object3D.rotation = this.el.object3D.lookAt(camera.object3D.position);
+      
+    const screenPos = toScreenPosition(this.el.object3D, this.canvas)
+    const scale = getScreenScale(1/this.parent.position.length())*4;
+  
+    this.shadow.style.transform = `translate(-50%, -50%) translate(${screenPos.x}px,${screenPos.y}px) scale(${scale})`;
+    if(this.parent.visible == true){
+      this.shadow.style.display="flex";
+    }else{
+      this.shadow.style.display="none";
+    }
+      },
   });
+
+  //toScreenPosition from https://stackoverflow.com/questions/27409074/converting-3d-position-to-2d-screen-position-r69
+
+  function toScreenPosition(obj,  canvas)
+{
+    var vector = new THREE.Vector3();
+
+    var widthHalf = 0.5*canvas.width;
+    var heightHalf = 0.5*canvas.height;
+
+    obj.updateMatrixWorld();
+    vector.setFromMatrixPosition(obj.matrixWorld);
+    vector.project(AFRAME.scenes[0].camera);
+
+    vector.x = ( vector.x * widthHalf ) + widthHalf;
+    vector.y = - ( vector.y * heightHalf ) + heightHalf;
+
+    return { 
+        x: vector.x,
+        y: vector.y
+    };
+
+};
+
+//getScreenScale from https://stackoverflow.com/questions/13350875/three-js-width-of-view/13351534#13351534
+function getScreenScale(dist){
+  var camera = AFRAME.scenes[0].camera;
+  var vFOV = THREE.Math.degToRad( camera.fov ); // convert vertical fov to radians
+  var height = 2 * Math.tan( vFOV / 2 ) * dist; // visible height
+  var width = height * camera.aspect;           // visible width
+
+  return width;
+}
---
+++ js/mrcp-device.js
@@ -0,0 +1,54 @@
+var characteristic_tx;
+var characteristic_rx;
+var device;
+var button = document.querySelector('#device_button');
+var led_on;
+
+async function BLEconnect(){
+  try {
+    console.log('Requesting Bluetooth Device...');
+    device  = await navigator.bluetooth.requestDevice({
+        //filters: [{services: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e']}]});
+        acceptAllDevices: true, optionalServices: ['6e400001-b5a3-f393-e0a9-e50e24dcca9e']});
+  
+    console.log('Connecting to GATT Server...');
+    const server = await device.gatt.connect();
+  
+    console.log('Getting Service...');
+    const service = await server.getPrimaryService('6e400001-b5a3-f393-e0a9-e50e24dcca9e');
+  
+    console.log('Getting Characteristic TX...');
+    characteristic_tx = await service.getCharacteristic('6e400002-b5a3-f393-e0a9-e50e24dcca9e');
+
+    console.log('Getting Characteristic RX...');
+    characteristic_rx = await service.getCharacteristic('6e400003-b5a3-f393-e0a9-e50e24dcca9e');
+    characteristic_rx.addEventListener('characteristicvaluechanged', handleLedStatusChange);
+    characteristic_rx.startNotifications();
+    console.log('Done');
+  } catch(error) {
+    console.log(error);
+  }
+  return Promise.resolve();
+}
+
+
+async function buttonPress(){
+  if(device == null){
+    button.innerHTML = "Connecting";
+    await BLEconnect();
+  }else{
+    let encoder = new TextEncoder('utf-8');
+    let sendMsg = encoder.encode((led_on ? 'OFF' : 'ON').toString());
+    console.log('Writing Characteristic...');
+    button.innerHTML = 'Loading';
+    await characteristic_tx.writeValue(sendMsg);
+  }
+}
+
+function handleLedStatusChange(event){
+  var status = document.querySelector('#device_status');
+  let decoder = new TextDecoder("utf-8");
+  led_on = parseInt(decoder.decode(event.target.value)) == 1;
+  button.innerHTML = led_on ? 'OFF' : 'ON';
+  status.style.backgroundColor = led_on ? '#39cc39' : '#ff5757';
+}
